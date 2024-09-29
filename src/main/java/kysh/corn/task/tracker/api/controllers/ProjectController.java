c69d6d36772c5903391d645246cef29ba4648171
kushlenkov <kushlenkov.av@gmail.com>
2024-09-30 00:09:26 +0300

    work on ProjectController add NotFoundException, AckDto edit ProjectRepository

--- src/main/java/kysh/corn/task/tracker/api/controllers/ProjectController.java
+++ src/main/java/kysh/corn/task/tracker/api/controllers/ProjectController.java
@@ -1,18 +1,23 @@
 package kysh.corn.task.tracker.api.controllers;
 
+import kysh.corn.task.tracker.api.dto.AckDto;
 import kysh.corn.task.tracker.api.dto.ProjectDto;
 import kysh.corn.task.tracker.api.exceptions.BadRequestException;
+import kysh.corn.task.tracker.api.exceptions.NotFoundException;
 import kysh.corn.task.tracker.api.factories.ProjectDtoFactory;
 import kysh.corn.task.tracker.store.entities.ProjectEntity;
 import kysh.corn.task.tracker.store.repositories.ProjectRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -24,21 +29,88 @@ public class ProjectController {
 
     ProjectDtoFactory projectDtoFactory;
 
+    public static final String FETCH_PROJECT = "/api/projects";
     public static final String CREATE_PROJECT = "/api/projects";
+    public static final String EDIT_PROJECT = "/api/projects/{project_id}";
+    public static final String DELETE_PROJECT = "/api/projects/{project_id}";
 
-    @PostMapping(CREATE_PROJECT)
-    public ProjectDto createProject(@RequestParam String name) {
+    public static final String CREATE_OR_UPDATE_PROJECT = "/api/projects";
 
-        projectRepository
-                .findByName(name)
-                .ifPresent(project -> {
-                    throw new BadRequestException(String.format("Project \"%s\" already exist.", name));
+    @GetMapping(FETCH_PROJECT)
+    public List<ProjectDto> fetchProject(
+            @RequestParam(value = "prefix_name", required = false) Optional<String> optionalPrefixName) {
+
+        optionalPrefixName = optionalPrefixName.filter(prefixName -> !prefixName.trim().isEmpty());
+
+        Stream<ProjectEntity> projectStream = optionalPrefixName
+                .map(projectRepository::streamAllByNameStartsWithIgnoreCase)
+                .orElseGet(projectRepository::streamAll);
+
+
+        return projectStream
+                .map(projectDtoFactory::makeProjectDro)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping(CREATE_OR_UPDATE_PROJECT)
+    public ProjectDto createOrUpdateProject(
+            @RequestParam(value = "project_id", required = false) Optional<Long> optionalProjectId,
+            @RequestParam(value = "project_name", required = false) Optional<String> optionalProjectName
+            // Another params...
+            ) {
+
+        optionalProjectName = optionalProjectName.filter(projectName -> !projectName.trim().isEmpty());
+
+        boolean isCreate = !optionalProjectId.isPresent();
+
+        if (isCreate && !optionalProjectName.isPresent()) {
+            throw new BadRequestException("Project name can't be empty.");
+        }
+
+        final ProjectEntity project = optionalProjectId
+                .map(this::getProjectOrThrowException)
+                .orElseGet(() -> ProjectEntity.builder().build());
+
+        optionalProjectName
+                .ifPresent(projectName -> {
+
+                    projectRepository
+                            .findByName(projectName)
+                            .filter(anotherProject -> !Objects.equals(anotherProject.getId(), project.getId()))
+                            .ifPresent(anotherProject -> {
+                                throw new BadRequestException(
+                                        String.format("Project \"%s\" already exist.", projectName)
+                                );
+                            });
+
+                    project.setName(projectName);
                 });
 
+        final ProjectEntity savedProject = projectRepository.saveAndFlush(project);
 
+        return projectDtoFactory.makeProjectDro(savedProject);
+    }
 
-        //TODO: uncomment and insert entity in method
-        return null;
-//        return projectDtoFactory.makeProjectDro();
+    @DeleteMapping(DELETE_PROJECT)
+    public AckDto deleteProject(@PathVariable("project_id") Long projectId) {
+
+        getProjectOrThrowException(projectId);
+
+        projectRepository.deleteById(projectId);
+
+        return AckDto.makeDefault(true);
+    }
+
+    private ProjectEntity getProjectOrThrowException(Long projectId) {
+        return projectRepository
+                .findById(projectId)
+                .orElseThrow(() ->
+                        new NotFoundException(
+                                String.format(
+                                        "Project with this Id \"%s\" doesn't exist.",
+                                        projectId
+                                )
+                        )
+                );
     }
 }
--- src/main/java/kysh/corn/task/tracker/store/repositories/ProjectRepository.java
+++ src/main/java/kysh/corn/task/tracker/store/repositories/ProjectRepository.java
@@ -4,7 +4,12 @@ import kysh.corn.task.tracker.store.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
     Optional<ProjectEntity> findByName(String name);
+
+    Stream<ProjectEntity> streamAll();
+
+    Stream<ProjectEntity> streamAllByNameStartsWithIgnoreCase(String prefixName);
 }
